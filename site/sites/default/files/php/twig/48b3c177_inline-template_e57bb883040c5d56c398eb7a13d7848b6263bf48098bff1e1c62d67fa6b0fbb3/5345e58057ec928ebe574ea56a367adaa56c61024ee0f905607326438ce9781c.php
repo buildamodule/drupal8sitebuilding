bedd20e4a4906ff7b409d145da329df7a0e4f1da
Chris Shattuck <ashattuc@gmail.com>
2016-07-19 11:19:13 -0600

    Adds snapshot for beginning of chapter 7

---
+++ site/sites/default/files/php/twig/48b3c177_inline-template_e57bb883040c5d56c398eb7a13d7848b6263bf48098bff1e1c62d67fa6b0fbb3/5345e58057ec928ebe574ea56a367adaa56c61024ee0f905607326438ce9781c.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {# inline_template_start #}Contact {{ field_first_name }} */
+class __TwigTemplate_f55d03a332e319113a835df39e7d8da59af28274cb4fcb6f2459823fadc37a56 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "Contact ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["field_first_name"]) ? $context["field_first_name"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}Contact {{ field_first_name }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}Contact {{ field_first_name }}*/
